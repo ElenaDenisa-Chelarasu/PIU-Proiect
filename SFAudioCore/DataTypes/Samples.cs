b1f1017fdeab1899158fb61340d2282268826465
Marioalexsan <mironalex@hotmail.com>
2022-10-16 21:15:54 +0300

    Initial project

---
+++ SFAudioCore/DataTypes/Samples.cs
@@ -0,0 +1,11 @@
+﻿namespace SFAudioCore.DataTypes;
+
+public class IntAudio
+{
+    public Memory<uint> Data { get; set; } = Array.Empty<uint>();
+}
+
+public class FloatAudio
+{
+    public Memory<float> Data { get; set; } = Array.Empty<float>();
+}
