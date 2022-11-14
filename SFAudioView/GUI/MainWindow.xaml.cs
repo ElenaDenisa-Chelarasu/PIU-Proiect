9690cdc43a4d74300fee0013cbcf78e9b390dcb3
ElenaDenisa-Chelarasu <100299021+ElenaDenisa-Chelarasu@users.noreply.github.com>
2022-11-14 20:10:34 +0200

    Implemeted the opening and saving in a file of a .wav file

--- SFAudioView/GUI/MainWindow.xaml.cs
+++ SFAudioView/GUI/MainWindow.xaml.cs
@@ -13,6 +13,9 @@ using System.Windows.Media.Imaging;
 using System.Windows.Media.TextFormatting;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.IO;
+using Microsoft.Win32;
+using NAudio.Wave;
 
 namespace SFAudio;
 /// <summary>
@@ -31,8 +34,47 @@ public partial class MainWindow : Window
         MessageBox.Show("Hello, world!");
     }
 
+    private DirectSoundOut? output = null;
+    private WaveFileReader? wave = null;
+
+    private void DisposeWave()
+    {
+        if (output !=null)
+        {
+            if (output.PlaybackState == PlaybackState.Playing)
+            {
+                output.Stop();
+            }
+            output.Dispose();
+            output = null;
+        }
+        if (wave != null)
+        {
+            wave.Dispose();
+            waveMenuItem = null;
+        }
+    }
+    /// <summary>
+    /// Deschide un fisier de tip wav si ruleaza impicit
+    /// TO DO: Sa nu se randeze implicit, ci doar sa deschida si sa salveze acest state, 
+    /// pentru a pute fi folosit in randarea unei imagini Waveform
+    /// </summary>
     private void openMenuItem_Click(object sender, RoutedEventArgs e)
     {
+        OpenFileDialog open = new OpenFileDialog();
+        open.Filter = "Wave File (*.wav)|*.wav;";
+        if(open.ShowDialog() != true)
+        {
+            return;
+        }
+        
+        DisposeWave();
 
+        wave = new WaveFileReader(open.FileName);
+        output = new DirectSoundOut();
+        output.Init(new WaveChannel32(wave));
+        output.Play();
+
+        mainBarGui.pauseButton.IsEnabled = true;
     }
 }
